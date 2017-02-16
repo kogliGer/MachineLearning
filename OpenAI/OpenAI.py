1427ceadcd14a37be7035e897c33438ac4277d33
kogliGer <kogli2000@gmail.com>
2017-02-16 16:40:39 +0100

    OpenAi cartPole RL agent added

--- OpenAI/OpenAI.py
+++ OpenAI/OpenAI.py
@@ -1,22 +1,58 @@
+import tensorflow as tf
 import gym
-import time
-import RLAgent as agent
+import numpy as np
 
-gamma = 0.8
-learningRate = 0.01
+gamma = 0.7
+epsilon = 0.5
+iterations = 10000
 
+x = tf.placeholder(tf.float32, shape=[None, 4])    #state
+y = tf.placeholder(tf.float32, shape=[None, 2])    #action
 
+#4 hidden neurons
+W = tf.Variable(tf.random_normal([4, 2]))
+b = tf.Variable(tf.random_normal([2]))
 
+output = tf.transpose(tf.sigmoid(tf.matmul(x, W) + b))
+loss = tf.reduce_sum(tf.square(output - y))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(loss)
+
+init = tf.global_variables_initializer()
+
+#setup environment
 environment = gym.make("CartPole-v0")
-observation, reward, done, info = environment.reset()
-print(environment.action_space)
-agent.feedForward([1, 1, 1, 2], 2)
-for i in range(200):
-    action = agent.learn(observation, reward)
-    observation, reward, done, info = environment.step(action)
-    environment.render()
-    if done:
-        #restart the environment
-        observation, reward, done, info = environment.reset()
-    else:
-        action = agent.learn(observation, reward)
+newState = np.empty(shape=(1, 4), dtype='float32')
+lastState = [[]]
+newState[0] = environment.reset()
+
+with tf.Session() as sess:
+    for iteration in range(iterations):
+        print("iteration " + str(iteration))
+        sess.run(init)
+
+        netOutput = sess.run(output, feed_dict={x: newState})
+        if np.random.rand(1) < epsilon:
+            action = environment.action_space.sample()
+        else:
+
+            if(netOutput[0] > netOutput[1]):
+                action = 0
+            else:
+                action = 1
+        #save original state
+        lastState = newState
+        newState[0], reward, done, info = environment.step(action)
+        environment.render()
+        # calculate true future reward
+        Q1 = sess.run(output, feed_dict={x: newState})
+        maxQ1 = np.max(Q1)
+        targetQ = netOutput
+        targetQ[action, 0] = reward + gamma * maxQ1
+        # Train our network using target and predicted Q values
+        sess.run(loss, feed_dict={x: lastState, y: targetQ.T})
+        print(loss)
+        if done:
+            epsilon = 1. / ((iteration / 50) + 10)
+            print("epsilon = " + str(epsilon))
+            print(info)
+            newState[0] = environment.reset()
--- OpenAI/RLAgent.py
+++ OpenAI/RLAgent.py
@@ -4,19 +4,34 @@ gamma = 0.7
 
 
 x = tf.placeholder(tf.float32, shape=[1, 4])    #state
-y = tf.placeholder(tf.float32, shape=[1, 1])    #action
+y = tf.placeholder(tf.float32, shape=[4, 1])    #action
 
 #4 hidden neurons
 W = tf.Variable(tf.random_normal([4, 4]))
 b = tf.Variable(tf.random_normal([4, 1]))
 
 output = tf.sigmoid(tf.matmul(x, W) + b)
+prediction = tf.arg_max(output)
+loss = tf.reduce_sum(tf.square(output - y))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(loss)
 
-
+init = tf.initialize_all_variables()
 def learn(lastState, actionExecuted, reward, currentState):
-    #loss = tf.reduce_sum(((reward + gamma * tf._max())))
-    print("learning")
-    return 1
+    with tf.Session() as sess:
+        sess.run(init)
+                greedyAction, netOutput = sess.run([prediction, output], feed_dict={x: currentState})
+                #if np.random.rand(1) < e:
+                #    a[0] = env.action_space.sample()
+
+                # Obtain the Q' values by feeding the new state through our network
+                Q1 = sess.run(Qout, feed_dict={inputs1: np.identity(16)[s1:s1 + 1]})
+                # Obtain maxQ' and set our target value for chosen action.
+                maxQ1 = np.max(Q1)
+                targetQ = allQ
+                targetQ[0, a[0]] = r + y * maxQ1
+                # Train our network using target and predicted Q values
+                _, W1 = sess.run([updateModel, W], feed_dict={inputs1: np.identity(16)[s:s + 1], nextQ: targetQ})
+        return greedyAction
 
 
 def feedForward(state, action):
